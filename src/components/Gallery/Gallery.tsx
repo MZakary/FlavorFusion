bef29e204d57d0aa833dae0a6c853992e63b05fa
MZakary <zakary-mitrovic@hotmail.ca>
2024-04-24 21:24:37 -0400

    added new images for ginger zing

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -107,9 +107,9 @@ function Gallery({ title, itemsGiven, searchBar }: { title: string, itemsGiven: 
                 </div>
             </div>
             {displayRecipes.length === 0 ? (
-                <>
+                <div>
                     <Circles height="80" width="80" color="#e8d9b4" ariaLabel="circles-loading" wrapperStyle={{}} wrapperClass="loading" visible={true} />
-                </>
+                </div>
             ) : (
                 <div className="ProjectContainer">
                     {displayRecipes.map((recipe: Recipe) => (
